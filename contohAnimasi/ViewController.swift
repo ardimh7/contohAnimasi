ea2166252870e73b2e070d32c182defe8cbaf2f7
Ardi Muhammad <ardi@arubygames.com>
2019-05-17 08:57:47 +0700

    Initial Commit

---
+++ contohAnimasi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  contohAnimasi
+//
+//  Created by Ardi Muhammad Husen on 17/05/19.
+//  Copyright © 2019 Ardi Muhammad Husen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
